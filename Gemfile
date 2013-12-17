f3d7d3e90ecea8a838b98d5ffefabddef1926144
DmitryYakovischak <dmitro.yakovischak@gmail.com>
2013-12-17 23:01:06 +0200

    add validation on last name,first name and email

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.6'
 gem 'mysql2'
 gem 'pry'
 gem 'twitter-bootstrap-rails'
+gem 'validates_email_format_of'
 
 
 # Gems used only for assets and not required
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :email
 
-
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, email_format: { message: "doesn't look like an email address" }
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
